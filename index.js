0e75cddd36d96876a48c9169d6ffa061c7a31afb
Irene <irene1martin2menendez@gmail.com>
2021-05-05 13:36:27 +0100

    Added more than 1 number when clicked

--- index.js
+++ index.js
@@ -1,9 +1,21 @@
+let num1 = "";
+let num2 = [];
+let num3 = [];
+
+// for (var i = 0; i < document.querySelectorAll(".calcutation").length; i++) {
+//     document.querySelectorAll(".calcutation")[i].addEventListener("click", function () {
+//         event.preventDefault();
+//         document.getElementById("number").value = this.innerHTML;
+//     });
+// } code working
 
 
-for (var i = 0; i < document.querySelectorAll(".calcutation").length; i++) {
-
+for (let i = 0; i < document.querySelectorAll(".calcutation").length; i++) {
     document.querySelectorAll(".calcutation")[i].addEventListener("click", function () {
         event.preventDefault();
-        document.getElementById("number").value = this.innerHTML;
-    });
-}
+        num1 = this.innerHTML;
+        num2.push(num1);
+        console.log(num2);
+        document.getElementById("number").value = num2.join("");
+
+});}
